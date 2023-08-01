abac87927f516a4bebd11400c176f042411bf061
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-08-01 21:42:27 +0200

    Add logging and asserts

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,14 @@
-#include <test.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
-    print_int(42);
+    KFATAL("Everything's borked! %f", 3.14f);
+    KERROR("Everything's borked! %f", 3.14f);
+    KWARN("Everything's borked! %f", 3.14f);
+    KINFO("Everything's borked! %f", 3.14f);
+    KDEBUG("Everything's borked! %f", 3.14f);
+    KTRACE("Everything's borked! %f", 3.14f);
+    // KASSERT(1 == 0);
+    KASSERT_MSG(1 == 2, "DAZ NOT KOMPJUT!");
     return 0;
 }
