d6508a83071aa9b6ccd29fc496a7ab6bdd292195
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-08-01 22:09:13 +0200

    Start platform layer

---
+++ engine/src/platform/platform.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "defines.h"
+
+typedef struct platform_state {
+  void *internal_state;
+} platform_state;
+
+b8 platform_startup(platform_state *plat_state, const char *application_name,
+                    i32 x, i32 y, i32 width, i32 height);
+
+void platform_shutdown(platform_state *plat_state);
+
+b8 platform_pump_messages(platform_state *plat_state);
